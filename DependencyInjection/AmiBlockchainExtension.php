80a5f589e68a0d24ac26f9bcbaa47c45c2d745f0
Elvis Morales <elvismdev@gmail.com>
2018-03-25 21:35:20 -0700

    Enhanced bundle configuration and compatibility with SF4.

--- DependencyInjection/AmiBlockchainExtension.php
+++ DependencyInjection/AmiBlockchainExtension.php
@@ -5,7 +5,7 @@ namespace Ami\BlockchainBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -19,13 +19,14 @@ class AmiBlockchainExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+
+        $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('ami_blockchain.api_key', $config['api_key']);
-        $container->setParameter('ami_blockchain.service_url', $config['service_url']);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $definition = $container->getDefinition('ami_blockchain.blockchain');
+        $definition->setArgument(0, $config['api_key']);
+        $definition->addMethodCall('setServiceUrl', [$config['service_url']]);
     }
 }
